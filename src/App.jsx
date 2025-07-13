07e4b279171b913c0556e5c4f598f6bc418197d9
engabuu35 <arbudibudibudi@gmail.com>
2025-07-13 20:05:34 +0700

    edit app

--- src/App.jsx
+++ src/App.jsx
@@ -67,7 +67,6 @@ export default function App() {
             }
           />
 
-          {/* ✅ Tambahkan ini */}
           <Route
             path="/publications/view/:id"
             element={
